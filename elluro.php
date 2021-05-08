8b0a1d8fd2dce503debb9ffcb16fafc23dd9dfa4
elyluque <70907388+elyluque@users.noreply.github.com>
2021-05-08 17:55:33 +0200

    Add files via upload

---
+++ elluro.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Esta función sirve para ...
+ * 
+ * @param int $parametro1 Esta es la descripción del parametro 1
+ * @param int $parametro2 Esta es la descripción del parametro 2
+ * @return int Esta es la descripción del parametro que devuelve la función
+ * @internal Esta es la descripción de la documentación para desarrolladores
+ * @author Elisabeth Luque de la Rosa < elyluque@gmail.com>
+ * @version string 1.0.1
+ */
+function miFuncion($paramametro1,$parametro2) {
+    $parametro = 1+1;
+      return   $parametro;        
+}
+
+
+/**
+ * Esta función sirve para ...
+ * 
+ * @param string $parametro3 Esta es la descripción del parametro 3
+ * @param string $parametro4 Esta es la descripción del parametro 4
+ * @param string $parametro5 Esta es la descripción del parametro 5
+ * @return string
+ * @internal Esta es la descripción de la documentación para desarrolladores
+ * @author Elisabeth Luque de la Rosa < elyluque@gmail.com>
+ * @version string 1.0.1
+ */
+function miFuncion2($parametro3,$parametro4,$parametro5){
+    $variable = $parametro3.$parametro4.$parametro5;
+    return $variable;
+}
