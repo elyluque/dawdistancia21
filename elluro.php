ad15d5d0f8122523f1e75d768b6fcb6d3a749eab
Elisabeth Luque <elyluque@gmail.com>
2021-05-08 18:06:21 +0200

    eliminar etiquetas internal

--- elluro.php
+++ elluro.php
@@ -5,7 +5,7 @@
  * @param int $parametro1 Esta es la descripción del parametro 1
  * @param int $parametro2 Esta es la descripción del parametro 2
  * @return int Esta es la descripción del parametro que devuelve la función
- * @internal Esta es la descripción de la documentación para desarrolladores
+ *
  * @author Elisabeth Luque de la Rosa < elyluque@gmail.com>
  * @version string 1.0.1
  */
@@ -22,7 +22,7 @@ function miFuncion($paramametro1,$parametro2) {
  * @param string $parametro4 Esta es la descripción del parametro 4
  * @param string $parametro5 Esta es la descripción del parametro 5
  * @return string
- * @internal Esta es la descripción de la documentación para desarrolladores
+ *
  * @author Elisabeth Luque de la Rosa < elyluque@gmail.com>
  * @version string 1.0.1
  */
